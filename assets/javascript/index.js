3ebe835d7f7ab5bec6b1df9d77219ef24551bd46
Tyler <tyler.dins88@gmail.com>
2023-02-20 20:50:59 -0600

    create function to write readme file

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,67 +1,82 @@
 // TODO: Include packages needed for this application
-const inquirer = require('inquirer');
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generate = require("./utils/generateMarkdown.js");
 
 // TODO: Create an array of questions for user input
 const questions = [
     {
         type: "input",
-        question: "What is your project title?",
+        message: "What is your project title?",
         name: "title"
     },
     {
         type: "input",
-        question: "What would you like the description to say?",
+        message: "What would you like the description to say?",
         name: "description"
     },
     {
         type: "input",
-        question: "What are the installation instructions?",
+        message: "What are the installation instructions?",
         name: "instructions"
     },
     {
         type: "input",
-        question: "What is the usage information?",
+        message: "What is the usage information?",
         name: "usage"
     },
     {
         type: "input",
-        question: "What are the contribution guidelines?",
+        message: "What are the contribution guidelines?",
         name: "contribution"
     },
     {
         type: "input",
-        question: "What are the test instructions?",
+        message: "What are the test instructions?",
         name: "test"
     },
     {
         type: "list",
-        question: "Which licences would you like to include?",
+        message: "Which licences would you like to include?",
         name: "license",
         choices: ["Apache 2.0", "GNU v3.0", "MIT", "BSD 2-Clause", "Boost Software 1.0", "Creative Commons Zero v1",
             "Eclipse Public License", "BSD 3-New", "GNU Affero", "GNU General", "GNU Lesser", "Mozilla", "The Unlicense"]
     },
     {
         type: "input",
-        question: "What is your GitHub username?",
+        message: "What is your GitHub username?",
         name: "github"
     },
     {
         type: "input",
-        question: "What is your email address?",
+        message: "What is your email address?",
         name: "email"
     },
     {
         type: "input",
-        question: "Would you like a table of contents for the README.md?",
+        message: "Would you like a table of contents for the README.md?",
         name: "contents"
     }
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) { }
+function writeToFile(fileName, data) {
+    fileName = generate(data);
+
+    fs.writeFile("professional-README.md", fileName, (err) =>
+        err ? console.log(err) : console.log("professional-README sucessfully generated!")
+    );
+}
 
 // TODO: Create a function to initialize app
-function init() { }
+function init() {
+    inquirer
+        .prompt(questions)
+        .then((answers) => {
+            const fileName = "generate-README";
+            const readMeResponse = writeToFile(fileName, answers)
+        })
+}
 
 // Function call to initialize app
 init();
